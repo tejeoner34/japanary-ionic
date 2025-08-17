6bdd2f2fd4a1d2ad09f61f1b49b8645a07c9795c
tejeoner34 <atejezar@gmail.com>
2025-08-17 23:34:04 +0900

    feat: add firebase auth time gap spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonMenu } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonMenu,
+  IonSpinner,
+} from '@ionic/angular/standalone';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { AuthService } from './services/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, SideMenuComponent, IonMenu],
+  imports: [
+    IonSpinner,
+    IonApp,
+    IonRouterOutlet,
+    SideMenuComponent,
+    IonMenu,
+    AsyncPipe,
+  ],
 })
 export class AppComponent {
+  authService = inject(AuthService);
+  isLoading = this.authService.isFirebaseUserLoading$;
   constructor() {}
 }
